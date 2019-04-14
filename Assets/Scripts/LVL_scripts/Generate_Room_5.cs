a1d6a9bc952f8e677a5efc0ef463b421810dcbda
kurtisfisher <kfisher@mtu.edu>
2019-04-14 15:21:31 -0400

    Made Level 5

--- Assets/Scripts/LVL_scripts/Generate_Room_5.cs
+++ Assets/Scripts/LVL_scripts/Generate_Room_5.cs
@@ -9,7 +9,7 @@ public static class Generate_Room_5 {
 
         //This Room will show the player that they can move from one pink tile to the next
 
-        List<LevelEditor_2.TileCoord> emptyTiles = new List<LevelEditor_2.TileCoord>
+        List<LevelEditor_2.TileCoord> Chunk1EmptyTiles = new List<LevelEditor_2.TileCoord>
         {
             new LevelEditor_2.TileCoord(3, 1),
             new LevelEditor_2.TileCoord(3, 2),
@@ -19,30 +19,55 @@ public static class Generate_Room_5 {
             new LevelEditor_2.TileCoord(3, 6),
 
         };
-        Node[,] chunk1 = LevelEditor_2.createChunk(_map, new Color32(244, 173, 66, 255), 4, 9, emptyTiles);
-        Node[,] chunk2 = LevelEditor_2.createChunk(_map, Color.cyan, 2, 9);
-        Node[,] chunk3 = LevelEditor_2.createChunk(_map, Color.red, 2, 9);
+        Node[,] chunk1 = LevelEditor_2.createChunk(_map, new Color32(244, 173, 66, 255), 4, 9, Chunk1EmptyTiles);
+        Node[,] chunk2 = LevelEditor_2.createChunk(_map, Color.cyan, 2, 4);
+        Node[,] chunk3 = LevelEditor_2.createChunk(_map, Color.red, 2, 4);
 
         LevelEditor_2.createTwoWayLink(
                 chunk1,
                 chunk2,
-                new LevelEditor_2.TileCoord(3, 7, false, true, false, false),
-                new LevelEditor_2.TileCoord(0, 7, false, false, false, true)
+                new LevelEditor_2.TileCoord(3, 7),
+                new LevelEditor_2.TileCoord(0, 2),
+                LevelEditor_2.Direction.East
                 );
         LevelEditor_2.createTwoWayLink(
                 chunk1,
                 chunk2,
-                new LevelEditor_2.TileCoord(3, 8, false, true, false, false),
-                new LevelEditor_2.TileCoord(0, 8, false, false, false, true)
+                new LevelEditor_2.TileCoord(3, 8),
+                new LevelEditor_2.TileCoord(0, 3),
+                LevelEditor_2.Direction.East
                 );
         LevelEditor_2.createTwoWayLink(
                 chunk1,
                 chunk3,
-                new LevelEditor_2.TileCoord(3, 0, false, true, false, false),
-                new LevelEditor_2.TileCoord(0, 0, false, false, false, true)
+                new LevelEditor_2.TileCoord(3, 0),
+                new LevelEditor_2.TileCoord(0, 0),
+                LevelEditor_2.Direction.East
                 );
+        LevelEditor_2.createOneWayLink(
+                chunk2,
+                chunk1,
+                new LevelEditor_2.TileCoord(0, 0),
+                new LevelEditor_2.TileCoord(3, 0),
+                LevelEditor_2.Direction.West
+                );
+        LevelEditor_2.createOneWayLink(
+                chunk3,
+                chunk1,
+                new LevelEditor_2.TileCoord(0, 2),
+                new LevelEditor_2.TileCoord(3, 7),
+                LevelEditor_2.Direction.West
+                );
+        LevelEditor_2.createOneWayLink(
+                chunk3,
+                chunk1,
+                new LevelEditor_2.TileCoord(0, 3),
+                new LevelEditor_2.TileCoord(3, 8),
+                LevelEditor_2.Direction.West
+                );
+
         LevelEditor_2.setSource(chunk1, new LevelEditor_2.TileCoord(2, 6));
-        LevelEditor_2.setTarget(chunk3, new LevelEditor_2.TileCoord(1, 4));
+        LevelEditor_2.setTarget(chunk3, new LevelEditor_2.TileCoord(1, 1));
 
         return _map;
     }
