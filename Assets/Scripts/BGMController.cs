5984b6f5c5cef441ee6b54ee10f5c2b7588c1d4c
Nate Blichfeldt <nblichfeldt@LAB.HUSKYGAMES.COM>
2020-02-08 15:51:59 -0500

    Added the FMOD integration and implemented a few of the background sounds to the game

---
+++ Assets/Scripts/BGMController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BGMController : MonoBehaviour {
+	private float endTime;
+	private float time;
+	private bool stop;
+		
+	// Use this for initialization
+	void Start () {
+		endTime = 65.0f;
+		time = 0.0f;
+		stop = false;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		
+		if (time > endTime && !stop) {
+			stop = true;
+			Destroy(gameObject, 5.0f);
+		}
+	}
+}
---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour {
+	private float endTime;
+	private float time;
+	public Object BGM;
+
+	// Use this for initialization
+	void Start () {
+		endTime = 65.0f;
+		time = 0.0f;
+		Instantiate(BGM);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		time += Time.deltaTime;
+		
+		if (time > endTime) {
+			time -= endTime;
+			Instantiate(BGM);
+		}
+	}
+}
